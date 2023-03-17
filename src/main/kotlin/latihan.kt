ba881c0fb6d64f3d8f44510e9e399cf7e7daec2f
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 14:15:10 +0700

    latihan data 1a

--- src/main/kotlin/latihan.kt
+++ src/main/kotlin/latihan.kt
@@ -1,11 +1,25 @@
+import java.nio.charset.CharsetEncoder
+
 fun main () {
-    println ("Hello world")
-    print ("Chaen")
+//    pertemuan 1
+//    println ("Hello world")
+//    print ("Chaen")
+//
+//    print ("pekerjaan : ")
+//    val pekerjaan = readLine()
+//    print ("Alamat : ")
+//    val alamat = readLine()
+//
+//    println("pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
 
-    print ("pekerjaan : ")
-    val pekerjaan = readLine()
-    print ("Alamat : ")
-    val alamat = readLine()
+    //pertemuan 2
+    fun main (args: Array<String>){
+    var namaLengkap: String = "Chaen"
+    var alamat: String
+    alamat = "yogyakarta"
+    var umur = 18
 
-    println("pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
+    println(namaLengkap)
+    println(alamat)
+    println(umur)
 }
